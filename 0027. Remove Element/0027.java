01fff5f1ab134eaca8daaa15a23f4972a6507aac
Asım <aliasimcoskun@gmail.com>
2025-02-06 14:56:18 +0300

    java and c

---
+++ 0027. Remove Element/0027.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        // I'm gonna try two pointer
+        int j=0;
+        for (int i=0; i<nums.length; i++){
+            if(nums[i]==val){
+                continue;
+            }
+            nums[j++]=nums[i];
+        }
+        return j;
+    }
+}
